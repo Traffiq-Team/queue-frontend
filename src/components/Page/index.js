d3409e448ff85f358b750089e64fe9bf6b12c9ba
chefong <ericong18@gmail.com>
2021-09-26 13:11:38 -0700

    Reorganize page and add footer

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,9 +3,12 @@ import styles from './styles.module.css';
 
 const Page = ({ children }) => {
   return (
-    <main className={styles.page}>
-      {children}
-    </main>
+    <section className={styles.page}>
+      <main className={styles.main}>
+        {children}
+      </main>
+      <footer className={styles.footer}>powered by TraffiQ</footer>
+    </section>
   )
 };
 
