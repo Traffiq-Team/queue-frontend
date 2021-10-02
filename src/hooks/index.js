b1178b710ca14a798831bd0774022ab35496aa76
chefong <ericong18@gmail.com>
2021-10-02 10:31:04 -0700

    Create hook for detecting active tab

---
+++ src/hooks/index.js
@@ -0,0 +1,4 @@
+import useInterval from './useInterval';
+import useTabActive from './useTabActive';
+
+export { useInterval, useTabActive };
---
+++ src/hooks/useTabActive.js
@@ -0,0 +1,24 @@
+import { useState, useEffect } from 'react';
+
+const useTabActive = () => {
+  const [tabActive, setTabActive] = useState(!document.hidden);
+
+  const onVisibilityChange = () => {
+    if (document.visibilityState === 'visible') {
+      setTabActive(true);
+    } else {
+      setTabActive(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('visibilitychange', onVisibilityChange);
+    return () => {
+      document.removeEventListener('visibilitychange', onVisibilityChange);
+    }
+  }, [tabActive]);
+
+  return tabActive;
+};
+
+export default useTabActive;
